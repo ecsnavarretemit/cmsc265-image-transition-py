35cabc033b96f7fc635ce5eff2f5b4d6d541f82d
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2017-02-03 07:55:55 +0800

    Implement delaunay triangulation on the face

--- run.py
+++ run.py
@@ -7,13 +7,86 @@
 # Version 1.0.0-alpha1
 
 import os
-import app
+import cv2
+import dlib
+import numpy as np
+# import glob
+# from app.image import validate_dimensions
 
-out_dir = os.path.join(os.getcwd(), "out")
+ASSETS_PATH = os.path.join(os.getcwd(), "assets/img")
+PREDICTOR_PATH = os.path.join(os.getcwd(), "data/shape-predictor/shape_predictor_68_face_landmarks.dat")
+CASCADE_PATH = os.path.join(os.getcwd(), "data/opencv-cascades/haarcascades/haarcascade_frontalface_default.xml")
 
-if not os.path.exists(out_dir):
-  os.makedirs(out_dir)
+predictor = dlib.shape_predictor(PREDICTOR_PATH)
+cascade = cv2.CascadeClassifier(CASCADE_PATH)
 
-app.grayscale(os.path.join(os.getcwd(), "assets/img/leaf.jpg"), out_dir + "/grayscale.jpg")
+# Check if a point is inside a rectangle
+def rect_contains(rect, point):
+  if point[0] < rect[0]:
+    return False
+  elif point[1] < rect[1]:
+    return False
+  elif point[0] > rect[2]:
+    return False
+  elif point[1] > rect[3]:
+    return False
+
+  return True
+
+def get_landmarks(im):
+  rects = cascade.detectMultiScale(im, 1.3, 5)
+
+  # dereference list elements into separate variables
+  x, y, w, h = rects[0]
+
+  # convert all parameters to int doing so will prevent errors with incompatibility with boost-python
+  rect = dlib.rectangle(int(x), int(y), int(x + w), int(y + h))
+
+  # return a new numpy matrix
+  return np.matrix([[p.x, p.y] for p in predictor(im, rect).parts()])
+
+def get_points(im, landmarks):
+  # clone the image instance to preserve the original instance
+  im = im.copy()
+  points = []
+
+  for _, point in enumerate(landmarks):
+    pos = (point[0, 0], point[0, 1])
+
+    points.append(pos)
+
+  return points
+
+def draw_triangles(im, landmarks):
+  # clone the image instance to preserve the original instance
+  im = im.copy()
+  size = im.shape
+  rect = (0, 0, size[1], size[0])
+
+  subdiv = cv2.Subdiv2D(rect)
+  points = get_points(im, landmarks)
+
+  for _, point in enumerate(points):
+    subdiv.insert(point)
+
+  triangle_list = subdiv.getTriangleList()
+
+  for triangle in triangle_list:
+    point1 = (triangle[0], triangle[1])
+    point2 = (triangle[2], triangle[3])
+    point3 = (triangle[4], triangle[5])
+
+    if rect_contains(rect, point1) and rect_contains(rect, point2) and rect_contains(rect, point3):
+      cv2.line(im, point1, point2, (255, 255, 255), 1, cv2.LINE_AA, 0)
+      cv2.line(im, point2, point3, (255, 255, 255), 1, cv2.LINE_AA, 0)
+      cv2.line(im, point3, point1, (255, 255, 255), 1, cv2.LINE_AA, 0)
+
+  return im
+
+if __name__ == '__main__':
+  img1 = cv2.imread(ASSETS_PATH + '/img-1.jpg')
+  cv2.imshow('Result', draw_triangles(img1, get_landmarks(img1)))
+  cv2.waitKey(0)
+  cv2.destroyAllWindows()
 
 
