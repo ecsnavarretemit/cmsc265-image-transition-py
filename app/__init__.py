a2d970e124f4a23a8e29c12caa76097952355d15
Exequiel Ceasar Navarrete <esnavarrete1@up.edu.ph>
2017-02-06 05:05:28 +0800

    Remove default code and assets

--- app/__init__.py
+++ app/__init__.py
@@ -4,20 +4,4 @@
 # Licensed under MIT
 # Version 1.0.0-alpha1
 
-import os
-import cv2
-
-def grayscale(source, destination):
-  if not os.path.exists(source):
-    raise Exception("%s image does not exist" % source)
-
-  # read the image file
-  image = cv2.imread(source)
-
-  # process the image
-  gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-  # write the file
-  cv2.imwrite(destination, gray_image)
-
 
